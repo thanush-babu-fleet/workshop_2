8c606cc1ec341f7acf4fb92b21b50a08ff94c751
Thanush Babu <tbabu@fleetstudio.com>
2025-07-28 22:54:23 +0530

    bug fixes in enhanced features

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -30,9 +30,6 @@ router.get('/export', taskController.exportTasks);
 // GET /api/tasks/templates - Get all templates
 router.get('/templates', taskController.getTemplates);
 
-// GET /api/tasks/:id - Get a single task by ID
-router.get('/:id', validateTaskId, handleValidationErrors, taskController.getTask);
-
 // POST /api/tasks - Create a new task
 router.post('/', validateCreateTask, handleValidationErrors, taskController.createTask);
 
@@ -45,20 +42,23 @@ router.post('/templates', validateCreateTask, handleValidationErrors, taskContro
 // POST /api/tasks/from-template - Create task from template
 router.post('/from-template', taskController.createFromTemplate);
 
+// PATCH /api/tasks/bulk - Bulk update tasks
+router.patch('/bulk', taskController.bulkUpdateTasks);
+
+// DELETE /api/tasks/bulk - Bulk delete tasks
+router.delete('/bulk', taskController.bulkDeleteTasks);
+
+// GET /api/tasks/:id - Get a single task by ID
+router.get('/:id', validateTaskId, handleValidationErrors, taskController.getTask);
+
 // PUT /api/tasks/:id - Update a task
 router.put('/:id', validateUpdateTask, handleValidationErrors, taskController.updateTask);
 
 // PATCH /api/tasks/:id - Partially update a task
 router.patch('/:id', validateUpdateTask, handleValidationErrors, taskController.updateTask);
 
-// PATCH /api/tasks/bulk - Bulk update tasks
-router.patch('/bulk', taskController.bulkUpdateTasks);
-
 // DELETE /api/tasks/:id - Delete a task
 router.delete('/:id', validateTaskId, handleValidationErrors, taskController.deleteTask);
-
-// DELETE /api/tasks/bulk - Bulk delete tasks
-router.delete('/bulk', taskController.bulkDeleteTasks);
 
 // PATCH /api/tasks/:id/complete - Mark task as completed
 router.patch('/:id/complete', validateTaskId, handleValidationErrors, taskController.completeTask);
